1eb0b1695a4989787d6e9a0b1eeba1d2c26d2231
Alexander Schoch <schochal@student.ethz.ch>
2025-03-20 11:06:06 +0100

    remove js from old views

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -9,20 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
-        <x-nav />
-
-        <main>
-            <x-hero />
-
-            <div class="max-w-[1200px] mx-auto relative p-4 w-full">
-                <x-steps />
-            </div>
-        </main>
-
-        <script src="js/hero.js"></script>
     </body>
 </html>
