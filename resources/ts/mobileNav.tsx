6f865bc7da3e15d7efe4cf9f287d4ea49ccbe7fe
Alexander Schoch <schochal@student.ethz.ch>
2025-03-31 00:25:03 +0200

    some mobile clients have a larger window.innerHeight than actual height

--- resources/ts/mobileNav.tsx
+++ resources/ts/mobileNav.tsx
@@ -18,10 +18,13 @@ export default function MobileNav() {
     console.log(opened);
 
     useEffect(() => {
-        if(opened)
+        if(opened) {
             document.body.classList.add('overflow-y-hidden');
-        else
+            document.body.classList.add('h-screen');
+        } else {
             document.body.classList.remove('overflow-y-hidden');
+            document.body.classList.remove('h-screen');
+        }
     }, [opened]);
 
     useEffect(() => {
@@ -61,20 +64,20 @@ export default function MobileNav() {
                     ))}
                 </ul>
                 {imprint.source && (
-                    <ul class="flex flex-col gap-4">
+                    <ul class="flex flex-col gap-3 text-sm">
                         <li>
                             <p class="flex gap-2 items-center">
                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-copyright"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M14 9.75a3.016 3.016 0 0 0 -4.163 .173a2.993 2.993 0 0 0 0 4.154a3.016 3.016 0 0 0 4.163 .173" /></svg>
                                 <span>{imprint['copyright']['year']}, {imprint['copyright']['author']}, {imprint['copyright']['place']}</span>
                             </p>
                         </li>
-                        <li class="flex gap-2 items-center">
+                        <li class="flex items-center gap-2">
                             {imprint['author']['label']
                                 .replace("[name]", ";;")
                                 .replace("[heart]", "!!")
                                 .split(';')
                                 .flatMap(p => p === ''
-                                    ? <a href={imprint['author']['url']} target="_blank" class="underline">{imprint['author']['name']}</a>
+                                    ? <a href={imprint['author']['url']} target="_blank" class="underline whitespace-nowrap">{imprint['author']['name']}</a>
                                     : p.split('!').map(r =>
                                         r === '' ? <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="text-red-600 icon icon-tabler icons-tabler-filled icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" /></svg> : r
                                     )
