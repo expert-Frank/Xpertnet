212a5dab3ad68ff529c9f09960c5663708d1eaf8
Alexander Schoch <schochal@student.ethz.ch>
2025-03-30 19:51:41 +0200

    improve performance of mobile nav

--- resources/ts/mobileNav.tsx
+++ resources/ts/mobileNav.tsx
@@ -4,29 +4,29 @@ import { createRoot } from 'react-dom/client';
 export default function MobileNav() {
     const [opened, setOpened] = useState<boolean>(false);
 
-    useEffect(() => {
-        if(opened)
-            document.body.classList.add('overflow-y-hidden');
-        else
-            document.body.classList.remove('overflow-y-hidden');
-    }, [opened]);
+    //useEffect(() => {
+    //    if(opened)
+    //        document.body.classList.add('overflow-y-hidden');
+    //    else
+    //        document.body.classList.remove('overflow-y-hidden');
+    //}, [opened]);
 
     return (
         <>
             <button onClick={() => setOpened(!opened)}>
                 <div className="relative rotate-0 duration-500 cursor-pointer w-4 h-4">
                     <span
-                        className={`block absolute h-[1px] bg-black dark:bg-neutral-100 left-0 duration-250 w-full ${opened ? "rotate-135 top-[8px]" : "rotate-0 top-[3px]"}`}
+                        className={`transition-transform block absolute h-[1px] bg-black dark:bg-neutral-100 left-0 duration-250 w-full ${opened ? "rotate-135 top-[8px]" : "rotate-0 top-[3px]"}`}
                     />
                     <span
-                        className={`block absolute h-[1px] bg-black dark:bg-neutral-100 rotate-0 duration-250 w-full top-[8px] ${opened ? "-left-[60px]" : "left-0"}`}
+                        className={`transition-transform block absolute h-[1px] bg-black dark:bg-neutral-100 rotate-0 duration-250 w-full top-[8px] ${opened ? "-translate-x-[60px]" : "translate-0"}`}
                     />
                     <span
-                        className={`block absolute h-[1px] bg-black dark:bg-neutral-100 left-0 duration-250 w-full ${opened ? "-rotate-135 top-[8px]" : "rotate-0 top-[13px]"}`}
+                        className={`transition-transform block absolute h-[1px] bg-black dark:bg-neutral-100 left-0 duration-250 w-full ${opened ? "-rotate-135 top-[8px]" : "rotate-0 top-[13px]"}`}
                     />
                 </div>
             </button>
-            <aside className={`absolute top-16 w-full z-30 duration-250 bg-white dark:bg-neutral-900 min-h-[calc(100vh-64px)] p-4 ${opened ? "left-0" : "-left-full"}`}>
+            <aside className={`left-0 transition-transform absolute top-16 w-full z-30 duration-250 bg-white dark:bg-neutral-900 min-h-[calc(100vh-64px)] p-4 ${opened ? "translate-x-0" : "-translate-x-full"}`}>
                 asdf
             </aside>
         </>
--- resources/views/livewire/nav.blade.php
+++ resources/views/livewire/nav.blade.php
@@ -7,19 +7,19 @@
     <!-- Mobile Navigation -->
     <!--<div class="flex flex-1 lg:hidden" id="mobile-nav"></div>-->
     <button wire:click="toggleNav" class="flex flex-1 lg:hidden">
-        <div class="relative rotate-0 duration-500 cursor-pointer w-4 h-4">
+        <div class="relative rotate-0 cursor-pointer w-4 h-4">
             <span
-                class="block absolute h-[1px] bg-black dark:bg-neutral-100 left-0 duration-250 w-full {{ $opened ? 'rotate-135 top-[8px]' : 'rotate-0 top-[3px]' }}"
+                class="transition-transform block absolute h-[1px] bg-black dark:bg-neutral-100 left-0 duration-250 w-full {{ $opened ? 'rotate-135 top-[8px]' : 'rotate-0 top-[3px]' }}"
             ></span>
             <span
-                class="block absolute h-[1px] bg-black dark:bg-neutral-100 rotate-0 duration-250 w-full top-[8px] {{ $opened ? '-left-[60px]' : 'left-0' }}"
+                class="transition-transform block absolute h-[1px] bg-black dark:bg-neutral-100 rotate-0 duration-250 w-full top-[8px] {{ $opened ? '-translate-x-[60px]' : 'transition-x-0' }}"
             ></span>
             <span
-                class="block absolute h-[1px] bg-black dark:bg-neutral-100 left-0 duration-250 w-full {{ $opened ? '-rotate-135 top-[8px]' : 'rotate-0 top-[13px]' }}"
+                class="transition-transform block absolute h-[1px] bg-black dark:bg-neutral-100 left-0 duration-250 w-full {{ $opened ? '-rotate-135 top-[8px]' : 'rotate-0 top-[13px]' }}"
             ></span>
         </div>
     </button>
-    <aside class="absolute top-16 w-full z-30 duration-250 bg-white dark:bg-neutral-900 min-h-[calc(100vh-64px)] p-4 {{ $opened ? 'left-0' : '-left-full' }}">
+    <aside class="transition-transform absolute left-0 top-16 w-full z-30 duration-250 bg-white dark:bg-neutral-900 min-h-[calc(100vh-64px)] p-4 {{ $opened ? 'translate-x-0' : '-translate-x-full' }}">
         asdf
     </aside>
 
