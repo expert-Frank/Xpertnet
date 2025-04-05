5e1922edf94a1f2428fcc0728a1ca7854a97f7a7
Alexander Schoch <schochal@student.ethz.ch>
2025-04-05 18:11:06 +0200

    add "under construction" banner

--- lang/de/hero.php
+++ lang/de/hero.php
@@ -4,4 +4,6 @@ return [
     "welcome" => "Willkommen bei",
     "solution" => "Die passgenaue Internetlösung für Ihr Zuhause und Ihr Business!",
     "description" => "Ob Sie einen schnellen und stabilen Internetzugang für Ihr Zuhause oder für Ihr Unternehmen suchen, bei uns finden Sie maßgeschneiderte Angebote für alle Anforderungen. Wählen Sie zwischen VDSL, Fiber (Glasfaser) oder klassischen DSL-Lösungen und profitieren Sie von höchster Qualität und zuverlässiger Leistung.",
+    "construction" => "In Entwicklung",
+    "constructionDesc" => "Diese Webseite befindet sich noch in der Entwicklung und ist daher noch nicht vollständig. Ende April 2025 präsentieren wir Ihnen aber stolz unsere fertige Webseite!"
 ];
--- lang/en/hero.php
+++ lang/en/hero.php
@@ -4,4 +4,6 @@ return [
     "welcome" => "Welcome to",
     "solution" => "The perfect internet fit for your home and your business!",
     "description" => "Whether you are looking for fast and stable Internet access for your home or for your company, we have customised offers for all requirements. Choose between VDSL, fibre optic or classic DSL solutions and benefit from the highest quality and reliable performance.",
+    "construction" => "Under Construction",
+    "constructionDesc" => "This website is currently being built and is therefore not feature complete yet. We'll be proud to present our finished website at the end of April 2025!"
 ];
